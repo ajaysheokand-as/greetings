233218f24acad0b1ebd0da39e5719138d72c7b9f
Deepak7009 <deepak7009verma@gmail.com>
2023-12-26 10:44:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [names, setNames] = useState('');
+    const [displayedNames, setDisplayedNames] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const namesArray = names.split(',').map(name => name.trim());
+        setDisplayedNames(namesArray);
+    };
+    return (
+        <>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="names">To:</label>
+                    <input
+                        type="text"
+                        id="names"
+                        name="names"
+                        value={names}
+                        onChange={(e) => setNames(e.target.value)}
+                        required
+                    />
+                    <label htmlFor="names">From:</label>
+                    <input
+                        type="text"
+                        id="names"
+                        name="names"
+                        // value={names}
+                        // onChange={(e) => setNames(e.target.value)}
+                        // required
+                    />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+
+                <div>
+                    {displayedNames.map((name, index) => (
+                        <h4 key={index}>{name}</h4>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
