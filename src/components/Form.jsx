1be7a5a074d15772c0534484f6eeaa1863746cc0
Deepak7009 <deepak7009verma@gmail.com>
2023-12-29 17:16:38 +0530

    new

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import FriendList from '../components/FriendList';
 import './form.css';
-import { Link } from 'react-router-dom';
-import new_year from '../images/good-morning.gif'
-import new_year2 from '../images/boldog.gif'
-import light from '../images/lights.gif'
-import bottom_img from '../images/superchat.gif'
-import bottom_img2 from '../images/sbenv.gif'
+import new_year from '../images/02.gif';
+import new_year2 from '../images/boldog.gif';
+import light from '../images/lights.gif';
+import bottom_img from '../images/superchat.gif';
+import bottom_img2 from '../images/sbenv.gif';
+import Messages from './Message';
 
 function Form() {
     const [names, setNames] = useState('');
     const [fromName, setFromName] = useState('');
     const [displayedNames, setDisplayedNames] = useState([]);
     const [displayedFromName, setDisplayedFromName] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        const namesArray = names.split(',').map(name => name.trim());
+        const namesArray = names.split(',').map((name) => name.trim());
         setDisplayedNames(namesArray);
         setDisplayedFromName(fromName);
+        setFormSubmitted(true);
     };
+
     return (
         <>
             <div className='content'>
@@ -31,39 +34,49 @@ function Form() {
                 <div className='outer_div'>
                     <img src={light} alt='light' />
                     <div className='main'>
-                        <form>
-                            <label htmlFor="toNames">To:</label>
-                            <input
-                                type="text"
-                                id="toNames"
-                                name="toNames"
-                                value={names}
-                                onChange={(e) => setNames(e.target.value)}
-                                required
-                            />
-                            <label htmlFor="fromName">From:</label>
-                            <input
-                                type="text"
-                                id="fromName"
-                                name="fromName"
-                                value={fromName}
-                                onChange={(e) => setFromName(e.target.value)}
-                                required
-                            />
-                            <br />
-                            {/* <Link to='/friend'><button className='btn' onClick={handleSubmit}>Submit</button></Link> */}
-                            <div className='submit_btn'>
-                                <button className='btn' onClick={handleSubmit}>Submit</button>
-                            </div>
-                        </form>
+                        {!formSubmitted && <form>
+                            {/* {!formSubmitted ? null : ( */}
+                            <>
+                                <label htmlFor='toNames'>To:</label>
+                                <input
+                                    type='text'
+                                    id='toNames'
+                                    name='toNames'
+                                    value={names}
+                                    onChange={(e) => setNames(e.target.value)}
+                                    required
+                                />
+                                <label htmlFor='fromName'>From:</label>
+                                <input
+                                    type='text'
+                                    id='fromName'
+                                    name='fromName'
+                                    value={fromName}
+                                    onChange={(e) => setFromName(e.target.value)}
+                                    required
+                                />
+                                <br />
+                                <div className='submit_btn'>
+                                    <a href={`git ?n=${names}&f=${fromName}`} target='_blank' className='btn'>
+                                        Create Your Own
+                                    </a>
+                                </div>
+                            </>
+                            {/* )} */}
+                        </form>}
+                        {formSubmitted && (
                         <div className='friend_names'>
+                            {/* <Messages/> */}
                             {displayedNames.map((name, index) => (
-                                <h4 key={index}><a href='https://first-wishes.com/happy-new-year?n'>{name}</a></h4>
+                                <h4 key={index}>
+                                    <a href={`https://first-wishes.com/happy-new-year?n=${name}`}>
+                                        {name}
+                                    </a>
+                                </h4>
                             ))}
-                            <h6 style={{ float: 'right' }}>{displayedFromName}</h6>
+                            <h6 style={{ float: 'right' }}>From: {displayedFromName}</h6>
                         </div>
-                        {/* <FriendList displayedNames={displayedNames} displayedFromName={displayedFromName} /> */}
-
+                        )}
                     </div>
                 </div>
                 <div className='bottom_logo'>
@@ -72,7 +85,7 @@ function Form() {
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Form
+export default Form;
